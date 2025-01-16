6b418eafad28a7c40eb74de945d372eb5dae4bc9
MINS2405 <9963@holbertonstudents.com>
2025-01-16 10:02:54 +0100

    ChatGPT - Introduction

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    result = 1
-    while n > 1:
-     result *= n
-    n -= 1  # Decrement n to avoid infinite loop
-    return result
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            input_number = int(sys.argv[1])
-            if input_number < 0:
-                print("Factorial is not defined for negative numbers.")
-            else:
-                print(factorial(input_number))
-        except ValueError:
-            print("Please provide a valid integer as input.")
-    else:
-        print("Usage: ./factorial.py <number>")
+# Start iterating from the second element to skip the script name
+for arg in sys.argv[1:]:
+    print(arg)
